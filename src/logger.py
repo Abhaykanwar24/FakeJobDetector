ad727f87ce7f044ae5ca8e1d1b4481cbf19a7115
Abhaykanwar24 <abhaykanwarsingh933@gmail.com>
2025-09-06 12:51:22 +0530

    Data ingestion done

--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,7 @@ import datetime as datetime
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 log_file = os.path.join(log_dir, LOG_FILE)
 
 
